2b18ed16466e29e817c31ad4fba143f1b9201c05
taozi <449141360@qq.com>
2019-09-05 10:59:16 +0800

    最大子序和

---
+++ 53.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int maxsum = 0;
+    int cursum = 0;
+    for(auto i : nums){
+        if(cursum > 0){
+            cursum += i;
+        }
+        else{
+            cursum = i;//若当前和小于0，从i位置重新开始计算和
+        }
+        maxsum = max(cursum, maxsum);
+    }
+    return maxsum;
+}
+
+int main()
+{
+    vector<int> v{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << maxSubArray(v) << endl;
+    return 0;
+}
