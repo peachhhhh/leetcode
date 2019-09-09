abd81e969531cb2bf73f8b9f6c0faf5bbcc179de
taozi <449141360@qq.com>
2019-09-09 10:28:56 +0800

    最小路径和

---
+++ 64.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//动态规划
+int minPathSum(vector<vector<int>> &grid)
+{
+    int m = grid.size(), n = grid[0].size();
+    int dp[m][n];
+    dp[0][0] = grid[0][0];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0)
+            {
+                dp[i][j] = dp[i][j - 1] + grid[i][j];
+            }
+            else if (j == 0)
+            {
+                dp[i][j] = dp[i - 1][j] + grid[i][j];
+            }
+            else
+            {
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
+int main()
+{
+    return 0;
+}
