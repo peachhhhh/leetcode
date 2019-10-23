de83595db843edb2bd9b1acc800e63f791c7fa45
taozi <449141360@qq.com>
2019-10-23 16:56:44 +0800

    二叉树的序列化与反序列化

---
+++ 297.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//递归实现
+class Codec
+{
+public:
+    string serialize(TreeNode *root) //前序遍历
+    {
+        if (!root)
+            return "NULL";
+        return to_string(root->val) + " " + serialize(root->left) + " " + serialize(root->right);
+    }
+
+    TreeNode *deserialize(string data)
+    {
+        istringstream in(data);
+        return helper(in);
+    }
+
+    TreeNode *helper(istringstream &in)
+    {
+        string val;
+        in >> val;
+        if (val == "NULL")
+            return nullptr;
+        auto root = new TreeNode(stoi(val));
+        root->left = helper(in);
+        root->right = helper(in);
+        return root;
+    }
+};
+
+void test()
+{
+    Codec c;
+    cout << c.serialize(c.deserialize("1 2 3 NULL NULL 4 5 NULL NULL NULL NULL "));
+}
+
+int main()
+{
+    test();
+    return 0;
+}
