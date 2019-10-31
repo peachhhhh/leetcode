51b7b65e3a9044540396c79637ffcc9bb58d4e3a
taozi <449141360@qq.com>
2019-10-31 11:09:44 +0800

    字符串解码

---
+++ 394.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+//栈
+string decodeString(string s)
+{
+    stack<string> ss;
+    stack<int> si;
+    string cur = "";
+    int n = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            n = n * 10 + s[i] - '0';
+        }
+        else if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+        {
+            cur = cur + s[i];
+        }
+        else if (s[i] == '[')
+        {
+            ss.push(cur);
+            si.push(n);
+            cur = "";
+            n = 0;
+        }
+        else
+        {
+            int tmp = si.top();
+            si.pop();
+            for (int j = 0; j < tmp; j++)
+            {
+                ss.top() += cur;
+            }
+            cur = ss.top();
+            ss.pop();
+        }
+    }
+    return cur;
+}
+
+void test()
+{
+    string s = "s2[ab]t3[cd4[ef]u]g";
+    cout << decodeString(s) << endl;
+}
+
+int main()
+{
+    test();
+    return 0;
+}
