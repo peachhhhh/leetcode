fc0963706b3b49ba5f7c5ff22e35015a8d845088
taozi <449141360@qq.com>
2019-11-06 16:03:23 +0800

    找到字符串中所有字母异位词

--- 438.cpp
+++ 438.cpp
@@ -17,14 +17,14 @@ vector<int> findAnagrams(string s, string p)
     int count = 0; //count用于计数s的当前窗口内和p相同字符的个数
     while (j < s.size())
     {
-        if (map[s[j]] > 0)
+        if (map[s[j]] > 0) //滑动完右指针，更新map数组
         {
             count++;
         }
         map[s[j]]--;
-        while (count == p.size())
+        while (count == p.size()) //如果相同字符数和字符串p长度相等，则开始移动窗口左端
         {
-            if (j - i + 1 == p.size())
+            if (j - i + 1 == p.size()) //如果滑动窗口和字符串p长度也相等，则找到答案
             {
                 res.push_back(i);
             }
@@ -33,7 +33,7 @@ vector<int> findAnagrams(string s, string p)
             {
                 count--;
             }
-            i++; //左指针往右滑动
+            i++; //左指针往右滑动，同时更新map数组
         }
         j++; //右指针往右滑动
     }
