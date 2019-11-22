ff01b0f5da8059e1f6487cd469a1c9cca8fc72f0
taozi <449141360@qq.com>
2019-11-22 16:09:54 +0800

    从中序与后序遍历序列构造二叉树

--- 106.cpp
+++ 106.cpp
@@ -13,15 +13,39 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-//迭代，栈，参考105题
+//递归，dfs，参考105题
+TreeNode *dfs(vector<int> &inorder, vector<int> &postorder, int il, int ir, int pl, int pr, unordered_map<int, int> &m)
+{
+    if (il > ir || pl > pr)
+    {
+        return NULL;
+    }
+    TreeNode *node = new TreeNode(postorder[pr]);
+    int len = ir - m[postorder[pr]];
+    node->left = dfs(inorder, postorder, il, ir - len - 1, pl, pr - len - 1, m);
+    node->right = dfs(inorder, postorder, ir - len + 1, ir, pr - len, pr - 1, m);
+    return node;
+}
+
 TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
+{
+    unordered_map<int, int> hashmap;
+    for (int i = 0; i < inorder.size(); i++)
+    {
+        hashmap[inorder[i]] = i;
+    }
+    return dfs(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1, hashmap);
+}
+
+//迭代，栈，参考105题，时间差不多，空间更节约
+TreeNode *buildTree2(vector<int> &inorder, vector<int> &postorder)
 {
     if (inorder.empty())
     {
         return NULL;
     }
     stack<TreeNode *> s;
-    TreeNode *root = new TreeNode(postorder[postorder.size() - 1]);
+    TreeNode *root = new TreeNode(postorder.back());
     s.push(root);
     for (int i = postorder.size() - 2, j = inorder.size() - 1; i >= 0; --i)
     {
